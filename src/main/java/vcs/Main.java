99903c8a3a97bba39dafe1e3704ec362c22268e9
rbbelka <rbbelka@yandex.ru>
2016-09-25 14:14:01 +0300

    main class

---
+++ src/main/java/vcs/Main.java
@@ -0,0 +1,12 @@
+package vcs;
+
+public class Main {
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.println("Incorrect input");
+            return;
+        }
+    }
+
+}
