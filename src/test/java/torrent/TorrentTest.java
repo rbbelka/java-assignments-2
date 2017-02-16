5af2ce74831cf1cd0648d8e300fb2fbf2b80107a
rbbelka <rbbelka@yandex.ru>
2017-02-16 21:09:49 +0300

    add temporary folder to test

--- src/test/java/torrent/TorrentTest.java
+++ src/test/java/torrent/TorrentTest.java
@@ -4,7 +4,9 @@ package torrent;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import torrent.client.Client;
 import torrent.client.ClientImpl;
 import torrent.exceptions.TorrentException;
@@ -14,6 +16,7 @@ import torrent.tracker.IdProvider;
 import torrent.impl.Server;
 import torrent.tracker.TrackerImpl;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
@@ -26,28 +29,18 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class TorrentTest {
-    private static final Path TEST_DIRECTORY = Paths.get("src", "test", "resources", "test");
-    private static final Path DIR_PATH1 = TEST_DIRECTORY.resolve("dir1");
-    private static final Path DIR_PATH2 = TEST_DIRECTORY.resolve("dir2");
+
+
+    @Rule
+    public final TemporaryFolder folder = new TemporaryFolder();
+
+    private static final String DIR_PATH1 = "dir1";
+    private static final String DIR_PATH2 = "dir2";
     private static final String FILE_NAME1 = "file1.txt";
     private static final String FILE_NAME2 = "file2.txt";
-    private static final Path FILE_PATH1 = DIR_PATH1.resolve(FILE_NAME1);
-    private static final Path FILE_PATH2 = DIR_PATH2.resolve(FILE_NAME2);
-    private static final long FILE_LENGTH1 = Constants.BLOCK_SIZE + 2;
-    private static final long FILE_LENGTH2 = 50;
     private static final byte[] LOCALHOST = new byte[] {127, 0, 0, 1};
     private static final long DELAY = 1000;
 
-    @Before
-    public void createTestDirectory() throws IOException {
-        if (!Files.exists(TEST_DIRECTORY))
-            Files.createDirectory(TEST_DIRECTORY);
-    }
-
-    @After
-    public void deleteTestDirectory() throws IOException {
-        FileUtils.deleteDirectory(TEST_DIRECTORY.toFile());
-    }
 
     @Test
     public void testList() throws IOException {
@@ -57,22 +50,24 @@ public class TorrentTest {
 
         Client client1 = new ClientImpl();
         client1.start(LOCALHOST);
-        Files.createDirectory(DIR_PATH1);
-        createFile(FILE_PATH1, FILE_LENGTH1);
-        client1.upload(FILE_PATH1.toString());
+        File dir1 = folder.newFolder(DIR_PATH1);
+        File file1 = folder.newFile(DIR_PATH1 + "/" + FILE_NAME1);
+        FileUtils.writeStringToFile(file1, "some string");
+        client1.upload(file1.getPath());
 
         Client client2 = new ClientImpl();
         client2.start(LOCALHOST);
         List<FileInfo> filesList = client2.getFilesList();
-        assertEquals(Collections.singletonList(new FileInfo(0, FILE_NAME1, FILE_LENGTH1)), filesList);
+        assertEquals(Collections.singletonList(new FileInfo(0, FILE_NAME1, file1.length())), filesList);
 
-        Files.createDirectory(DIR_PATH2);
-        createFile(FILE_PATH2, FILE_LENGTH2);
-        client2.upload(FILE_PATH2.toString());
+        File dir2 = folder.newFolder(DIR_PATH2);
+        File file2 = folder.newFile(DIR_PATH2 + "/" + FILE_NAME2);
+        FileUtils.writeStringToFile(file2, "some other string");
+        client2.upload(file2.getPath());
         List<FileInfo> filesList2 = client1.getFilesList();
         assertEquals(Arrays.asList(
-                new FileInfo(0, FILE_NAME1, FILE_LENGTH1),
-                new FileInfo(1, FILE_NAME2, FILE_LENGTH2)
+                new FileInfo(0, FILE_NAME1, file1.length()),
+                new FileInfo(1, FILE_NAME2, file2.length())
         ), filesList2);
 
         client1.stop();
@@ -88,25 +83,21 @@ public class TorrentTest {
 
         Client client1 = new ClientImpl();
         client1.start(LOCALHOST);
-        Files.createDirectory(DIR_PATH1);
-        createFile(FILE_PATH1, FILE_LENGTH1);
-        client1.upload(FILE_PATH1.toString());
+        File dir1 = folder.newFolder(DIR_PATH1);
+        File file1 = folder.newFile(DIR_PATH1 + "/" + FILE_NAME1);
+        FileUtils.writeStringToFile(file1, "test string");
+        client1.upload(file1.getPath());
 
         Client client2 = new ClientImpl();
         client2.start(LOCALHOST);
-        Files.createDirectory(DIR_PATH2);
+        File dir2 = folder.newFolder(DIR_PATH2);
+        File file2 = new File (folder.getRoot(), DIR_PATH2 + "/" + FILE_NAME1);
         Thread.sleep(DELAY);
-        client2.download(0, DIR_PATH2);
-        assertEquals(FILE_LENGTH1, DIR_PATH1.resolve(FILE_NAME1).toFile().length());
+        client2.download(0, dir2.toPath());
+        assertEquals(file1.length(), file2.length());
 
         client1.stop();
         client2.stop();
         server.stop();
-    }
-
-    private void createFile(Path filePath, long fileLength) throws IOException {
-        RandomAccessFile file = new RandomAccessFile(filePath.toFile(), "rw");
-        file.setLength(fileLength);
-        file.close();
     }
 }
